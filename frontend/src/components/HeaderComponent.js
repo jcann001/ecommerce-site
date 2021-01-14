a2a051bb1527f56e9e863362cdc5b4616ad7604e
Julian Cannon <cannonj55@gmail.com>
2021-01-14 04:51:10 -0500

    Added information

--- frontend/src/components/HeaderComponent.js
+++ frontend/src/components/HeaderComponent.js
@@ -9,10 +9,14 @@ class Header extends Component {
         super(props);
 
         this.state = {
-          isModalOpen: false
+          isModalOpen: false,
+          isRegOpen: false,
+          isCartOpen: false
         };
 
+        this.toggleRegistration = this.toggleRegistration.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
+        this.toggleCart = this.toggleCart.bind(this);
         // this.handleLogin = this.handleLogin.bind(this);
     }
 
@@ -22,6 +26,18 @@ class Header extends Component {
         });
     }
 
+    toggleRegistration() {
+        this.setState({
+            isRegOpen: !this.state.isRegOpen
+        });
+    }
+
+    toggleCart() {
+      this.setState({
+          isCartOpen: !this.state.isCartOpen
+      });
+  }
+
     // handleLogin(event) {
     //     alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
     //     this.toggleModal();
@@ -30,9 +46,9 @@ class Header extends Component {
 
     render() {
         return (
-            <React.Fragment>
-    <div className="Menu">
-    <Nav className="navbar navbar-expand navbar-light">
+    <React.Fragment>
+      <div className="Menu">
+      <Nav className="navbar navbar-expand navbar-light">
       <a className="navbar-brand" href="index.html"><img src="/img/leaficon.jpg" style={{width: '40px'}} alt=""/> </a>
       <a className="navbar-brand" href="index.html">GreenLeaf </a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -42,141 +58,92 @@ class Header extends Component {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <form className="searchform">
+            <Form className="searchform">
               <div className="form-group has-search">
                 <button id="searchButton" type="submit" className="fa fa-search form-control-feedback"></button>
                 <input type="text" className="form-control" placeholder="Search"/>
               </div>
-            </form>
+            </Form>
           </li>
         </ul>
       </div>
       <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul className="navbar-nav justify-content-end" id="accountLinks">
           <li className="nav-item">
-            <a className="fa fa-shopping-cart" data-toggle="modal" data-target="#modalCart"></a>
+            <a onClick={this.toggleCart} className="fa fa-shopping-cart" data-toggle="modal" data-target="#modalCart"></a>
           </li>
           <li className="nav-item active">
-            <a onClick={this.toggleModal} className="nav-link" id="registerLink" data-toggle="modal" data-target="#registerModal">Register</a>
+            <a onClick={this.toggleRegistration} className="nav-link" id="registerLink" data-toggle="modal" data-target="#registerModal">Register</a>
           </li>
           <li className="nav-item active">
-            <a className="nav-link" id="loginLink" data-toggle="modal" data-target="#loginModal">Login</a>
+            <a onClick={this.toggleModal} className="nav-link" id="loginLink" data-toggle="modal" data-target="#loginModal">Login</a>
           </li>
         </ul>
       </div>
     </Nav>
 
     <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-    <div id="registerModal" className="modal fade" role="dialog">
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <ModalHeader toggle={this.toggleModal}><h3 className="modal-title">Sign up</h3></ModalHeader>
-            <button type="button" className="close" data-dismiss="modal">&times;</button>
-          </div>
-          <ModalBody>
-          <div className="modal-body">
-            <div className="container-fluid">
-              <form>
-                <div className="form-row">
-                  <div className="form-group col-12">
-                    <label className="sr-only" for="loginFirstName">First Name</label>
-                    <input type="text" className="form-control form-control-sm" id="loginFirstName" placeholder="First Name"
-                      required/>
-                  </div>
-                  <div className="form-group col-12">
-                    <label className="sr-only" for="loginLastName">Last Name</label>
-                    <input type="text" className="form-control form-control-sm" id="loginLastName" placeholder="Last Name"
-                      required/>
-                  </div>
-                  <div className="form-group col-12">
-                    <label className="sr-only" for="loginEmail">Email address</label>
-                    <input type="email" className="form-control form-control-sm" id="loginEmail" placeholder="Email"
-                      required/>
-                  </div>
-                  <div className="form-group col-12">
-                    <label className="sr-only" for="loginPhoneNumber">Phone Number</label>
-                    <input type="email" className="form-control form-control-sm" id="loginPhoneNumber"
-                      placeholder="Phone Number (Optional)"/>
-                  </div>
-                  <div className="form-group col-12">
-                    <label className="sr-only" for="loginPassword">Password</label>
-                    <input type="password" className="form-control form-control-sm" id="loginPassword"
-                      placeholder="Password" required/>
-                  </div>
-                  <div className="col">
-                    <div className="form-check">
-                      <input className="form-check-input" type="checkbox"/>
-                      <label className="form-check-label"> Remember me</label>
-                    </div>
-                  </div>
-                </div>
-            
-                <div className="form-row">
-                  <button type="button" className="btn btn-secondary btn-sm ml-auto" data-dismiss="modal">Cancel</button>
-                  <button type="submit" className="btn btn-success btn-sm ml-1">Sign in</button>
-                </div>
-              </form>
-            </div>
-          </div>
-          </ModalBody>
-        </div>
-      </div>
-    </div>
+    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+    <ModalBody>
+        <Form onSubmit={values => this.handleSubmit(values)}>
+            <FormGroup>        
+                    <Input type="email" id="email" name="email" placeholder="Email"
+                    innerRef={input => this.email = input} />
+            </FormGroup>
+            <FormGroup>
+                                
+                    <Input type="password" id="password" name="password" placeholder="Password"
+                        innerRef={input => this.password = input} />
+            </FormGroup>
+            <FormGroup check>
+                <Label check>
+                    <Input type="checkbox" name="remember" 
+                    innerRef={input => this.remember = input} />
+                    Remember me
+                </Label>
+            </FormGroup> 
+            <Button onClick={this.toggleModal} type="button" className="btn btn-secondary btn-sm ml-auto" data-dismiss="modal">Cancel</Button>
+            <Button type="submit" className="btn btn-success btn-sm ml-1" value="submit" color="secondary">Submit</Button>
+        </Form>
+    </ModalBody>
     </Modal>
 
-    <div id="loginModal" className="modal fade" role="dialog">
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h3 className="modal-title">Login</h3>
-            <button type="button" className="close" data-dismiss="modal">&times;</button>
-          </div>
-          <div className="modal-body">
-            <div className="container-fluid">
-              <form>
-                <div className="form-row">
-                  <div className="form-group col-12">
-                    <label className="sr-only" for="loginEmail">Email address</label>
-                    <input type="email" className="form-control form-control-sm" id="loginEmail" placeholder="Email"/>
-                  </div>
-                  <div className="form-group col-12">
-                    <label className="sr-only" for="loginPassword">Password</label>
-                    <input type="password" className="form-control form-control-sm" id="loginPassword"
-                      placeholder="Password"/>
-                  </div>
-                  <div className="col">
-                    <div className="form-check">
-                      <input className="form-check-input" type="checkbox"/>
-                      <label className="form-check-label"> Remember me</label>
-                    </div>
-                  </div>
-                </div>
-                <div className="form-row">
-                  <button type="button" className="btn btn-secondary btn-sm ml-auto" data-dismiss="modal">Cancel</button>
-                  <button type="submit" className="btn btn-success btn-sm ml-1">Sign in</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Modal isOpen={this.state.isRegOpen} toggle={this.toggleRegistration}>
+    <ModalHeader toggle={this.toggleRegistration}>Register</ModalHeader>
+    <ModalBody>
+        <Form onSubmit={values => this.handleSubmit(values)}>
+            <FormGroup>        
+                    <Input type="text" id="firstname" name="firstname" placeholder="First Name"
+                    innerRef={input => this.firstname = input} />
+            </FormGroup>
+            <FormGroup>
+                                
+                    <Input type="text" id="lastname" name="lastname" placeholder="Last Name"
+                        innerRef={input => this.lastname = input} />
+            </FormGroup>
+            <FormGroup>        
+                    <Input type="email" id="email" name="email" placeholder="Email"
+                    innerRef={input => this.email = input} />
+            </FormGroup>
+            <FormGroup>        
+                    <Input type="tel" id="phone" name="phone" placeholder="Phone Number (Optional)"
+                    innerRef={input => this.phone = input} />
+            </FormGroup>
+            <FormGroup>        
+                    <Input type="password" id="password" name="password" placeholder="Password"
+                    innerRef={input => this.password = input} />
+            </FormGroup>
+            <Button onClick={this.toggleRegistration} type="button" className="btn btn-secondary btn-sm ml-auto" data-dismiss="modal">Cancel</Button>
+            <Button type="submit" className="btn btn-success btn-sm ml-1" value="submit" >Submit</Button>
+        </Form>
+    </ModalBody>
+    </Modal>
 
-    <div className="modal fade" id="modalCart" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-      aria-hidden="true">
-      <div className="modal-dialog" role="document">
-        <div className="modal-content">
-          
-          <div className="modal-header">
-            <h4 className="modal-title" id="myModalLabel">My cart</h4>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-          
-          <div className="modal-body">
-
+    <Modal isOpen={this.state.isCartOpen} toggle={this.toggleCart}>
+    <ModalHeader toggle={this.toggleCart}>My Cart</ModalHeader>
+    <ModalBody>
+        <Form onSubmit={values => this.handleSubmit(values)}>
+            <FormGroup>        
             <table className="table table-hover">
               <thead>
                 <tr>
@@ -188,38 +155,33 @@ class Header extends Component {
               </thead>
               <tbody>
                 <tr>
-                  <th scope="row">1</th>
+                  <td scope="row">1</td>
                   <td>Venus Fly Trap</td>
                   <td>$9.99</td>
                   <td><a><i className="fa fa-times"></i></a></td>
                 </tr>
                 <tr>
-                  <th scope="row">2</th>
+                  <td scope="row">2</td>
                   <td>Lavender</td>
                   <td>$9.99</td>
                   <td><a><i className="fa fa-times"></i></a></td>
                 </tr>
                 <tr className="total">
-                  <th scope="row"></th>
+                  <td scope="row"></td>
                   <td>Total</td>
                   <td>$19.98</td>
                   <td></td>
                 </tr>
               </tbody>
             </table>
-
-          </div>
-          
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-            <a href="checkout.html" className="btn btn-success">Checkout</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
-                
-            </React.Fragment>
+            </FormGroup>
+            <Button onClick={this.toggleCart} type="button" className="btn btn-secondary btn-sm ml-auto" data-dismiss="modal">Cancel</Button>
+            <Button type="submit" className="btn btn-success btn-sm ml-1" value="submit" >Checkout</Button>
+        </Form>
+    </ModalBody>
+    </Modal>
+    </div>    
+    </React.Fragment>
         );
     }
 }
---
+++ frontend/src/components/HomeComponent.js
@@ -0,0 +1,1 @@
+//holds carousel and main page and newsletter
